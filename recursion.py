92f57bbf594cfc27e6eb5d3dfacbba52da6a9c51
Hibou <namnguyen107@gmail.com>
2015-01-19 00:09:06 +0100

    ok

--- recursion.py
+++ recursion.py
@@ -1,5 +1,5 @@
 __author__ = 'hnng'
-
+import math
 
 def fibonancci(n):
     if n in [0, 1]:
@@ -14,6 +14,7 @@ def combinations_recursive(iterable):
     first = (iterable[0],)
     subset = combinations_recursive(iterable[1:])
     return [first + elem for elem in subset] + [first]
+
 
 def combinations(iterable, r):
     # combinations('ABCD', 2) --> AB AC AD BC BD CD
@@ -87,6 +88,7 @@ def colorized(pixels, i, j, new_color):
                     and pixels[new_i][new_j] == old_color:
                 to_be_computed.append((new_i, new_j))
 
+
 def test_colorized():
     m = [['X', 'X', 'X', 'X', 'X', 'X'],
          ['X', 'X', 'Y', 'Y', 'X', 'X'],
@@ -101,19 +103,87 @@ def test_colorized():
     for l in m:
         print l
 
-if __name__ == '__main__':
-    # for c in combinations('abcdefgh', 3):
-    # print c
 
-    m = [['X', 'X', 'X', 'X', 'X', 'X'],
-         ['X', 'X', 'Y', 'Y', 'X', 'X'],
-         ['X', 'X', 'X', 'Y', 'Y', 'X'],
-         ['X', 'X', 'X', 'X', 'X', 'X'],
-         ['X', 'X', 'X', 'X', 'X', 'X'],
-    ]
-    for l in m:
-        print l
-    colorized(m, 1, 2, 'Z')
-    print "============================="
-    for l in m:
-        print l
+def reverse_list(l):
+    if len(l) == 1:
+        return l
+    else:
+        return [l[-1]] + reverse_list(l[:-1])
+
+
+import turtle
+
+
+def tree(branchLen, t):
+    if branchLen > 5:
+        t.forward(branchLen)
+        t.right(20)
+        tree(branchLen - 15, t)
+        t.left(40)
+        tree(branchLen - 15, t)
+        t.right(20)
+        t.backward(branchLen)
+
+
+def test_tree():
+    t = turtle.Turtle()
+    myWin = turtle.Screen()
+    t.left(90)
+    t.up()
+    t.backward(100)
+    t.down()
+    t.color("green")
+    tree(75, t)
+    myWin.exitonclick()
+
+
+def moveTower(height, fromPole, toPole, withPole):
+    if height >= 1:
+        moveTower(height - 1, fromPole, withPole, toPole)
+        moveDisk(fromPole, toPole)
+        moveTower(height - 1, withPole, toPole, fromPole)
+
+
+def move_tower_iterative(height, from_pole, to_pole, with_pole):
+    stack_move_first = []
+    stack_move_after = []
+    height_first = height
+    while height_first >= 1:
+        stack_move_after.append((height_first - 1, from_pole, with_pole, to_pole))
+
+    pass
+
+
+def moveDisk(fp, tp):
+    print("moving disk from", fp, "to", tp)
+
+
+def hilber_curve():
+    t = turtle.Turtle()
+    myWin = turtle.Screen()
+    t.left(90)
+    t.up()
+    t.backward(100)
+    t.down()
+    t.color("green")
+    myWin.exitonclick()
+
+
+def pascal(n, spaces=0):
+    to_print = '   ' * spaces
+    if n == 0:
+        print to_print + '1'
+    else:
+        pascal(n - 1, spaces + 1)
+        for i, k in enumerate(range(n + 1)):
+            if k == 0:
+                to_print += str(math.factorial(n) / (math.factorial(i) * math.factorial(n - i)))
+            else:
+                to_print += '   ' + str(math.factorial(n) / (math.factorial(i) * math.factorial(n - i)))
+        print to_print
+
+
+
+
+if __name__ == '__main__':
+    pascal(15)
