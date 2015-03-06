ee903e12b40b1747f8e87f07747641db4c13f3b2
hnng <nam@derivexperts.com>
2015-03-06 18:40:36 +0100

    ok

--- arithmetic.hs
+++ arithmetic.hs
@@ -1,3 +1,4 @@
+import qualified Data.Map as Map  
 isPrime :: (Integral a) => a -> Bool
 isPrime n | n < 4 = n > 1
 isPrime n = all ((/=0).mod n) $ 2:3:[x + i | x <- [6,12..s], i <- [-1,1]]
@@ -61,3 +62,64 @@ internals Empty = 0
 internals (Branch x left right)
     | left == Empty && right == Empty = 0
     | otherwise = 1 + (internals left) + (internals right)
+
+
+atLevel Empty n = []
+atLevel tree n = f tree 1 n
+    where
+        f (Branch x left right) currentLevel level
+            | currentLevel == level = [x]
+            | otherwise = (f left (currentLevel + 1)  level) ++ (f right (currentLevel + 1)  level)
+
+tree64 = Branch 'n'
+                (Branch 'k'
+                        (Branch 'c'
+                                (Branch 'a' Empty Empty)
+                                (Branch 'h'
+                                        (Branch 'g'
+                                                (Branch 'e' Empty Empty)
+                                                Empty
+                                        )
+                                        Empty
+                                )
+                        )
+                        (Branch 'm' Empty Empty)
+                )
+                (Branch 'u'
+                        (Branch 'p'
+                                Empty
+                                (Branch 's'
+                                        (Branch 'q' Empty Empty)
+                                        Empty
+                                )
+                        )
+                        Empty
+                )
+
+
+layout tree =
+    let
+        shiftX x result = map (\ (a, (b, c)) -> (a, (b + x, c))) result
+        shiftY y result = map (\ (a, (b, c)) -> (a, (b, y + c))) result
+        f Empty = ([], 0)
+        f (Branch x left right) = ([(x, (xRoot, 1))] ++ (shiftY 1 (fst fleft)) ++ (shiftX xRoot (shiftY 1 (fst fright))),
+                                    (snd fright) + xRoot
+                                    )
+            where
+                fleft = f left
+                fright = f right
+                xRoot = (snd fleft) + 1
+    in fst $ f tree
+
+problem_31 = ways [1,2,5,10,20,50,100,200] !! 200
+  where ways [] = 1 : repeat 0
+        ways c@(coin:coins) =n 
+          where n = zipWith (+) (ways coins) ((replicate coin 0) ++ n)
+
+
+
+data MTree a = Node a [MTree a]
+        deriving (Eq, Show)
+
+nnodes (Node _ childs) = 1 + (sum $ map nnodes childs)
+
---
+++ finance.hs
@@ -0,0 +1,11 @@
+price asset volatility rate strike expiry nosteps = 
+    let timestep = expiry / nosteps
+        discountFactor = exp (-rate * (fromIntegral timestep))
+        temp1 = exp ((rate * volatility^2) * timestep)
+        temp2 = 0.5 * (discountFactor * temp1)
+        u = temp2 + sqrt (temp2^2 - 1)
+        d = 1 / u
+        p = ((exp (rate * (fromIntegral timestep)) - d)) / (u - d)
+    in 
+
+        0
