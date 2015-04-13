2be5a2106639b1b65468bede3c56d62908980dc2
hnng <nam@derivexperts.com>
2015-04-13 09:32:33 +0200

    ok

--- racket/algo.rkt
+++ racket/algo.rkt
@@ -10,22 +10,22 @@
               (next_prime current (* f (- next_p 2)) next_p)))))
   (next_prime null 1 2))
 
-(define (generate_sum list_num)
+(define (generate-sum list_num)
   (cond [(empty? list_num) null]
         [(empty? (rest list_num)) (list (first list_num))]
-        [else (let ([computed_rest (generate_sum (rest list_num))])
-                (cons (+ (first list_num) (first computed_rest)) computed_rest))]))
+        [else (let ([computed-rest (generate-sum (rest list_num))])
+                (cons (+ (first list_num) (first computed-rest)) computed-rest))]))
 
-(define (binary_search elem vector_nums)
-  (define (binary_search_slice elem vector_nums left right)
+(define (binary_search elem vector-nums)
+  (define (binary_search_slice elem vector-nums left right)
     (let ([mid (quotient (+ left right) 2)]) 
       (cond
-        [(= elem (vector-ref vector_nums right)) true]
-        [(= elem (vector-ref vector_nums left)) true]
-        [(> elem (vector-ref vector_nums right)) false]
-        [(< elem (vector-ref vector_nums left)) false]
-        [(= left right) (= elem (vector-ref vector_nums left))]
-        [(= elem (vector-ref vector_nums mid)) true]
-        [(< elem (vector-ref vector_nums mid)) (binary_search_slice elem vector_nums left mid)]
-        [else (binary_search_slice elem vector_nums mid right)])))
-  (binary_search_slice elem vector_nums 0 (sub1 (vector-length vector_nums))))
+        [(= elem (vector-ref vector-nums right)) true]
+        [(= elem (vector-ref vector-nums left)) true]
+        [(> elem (vector-ref vector-nums right)) false]
+        [(< elem (vector-ref vector-nums left)) false]
+        [(= left right) (= elem (vector-ref vector-nums left))]
+        [(= elem (vector-ref vector-nums mid)) true]
+        [(< elem (vector-ref vector-nums mid)) (binary_search_slice elem vector-nums left mid)]
+        [else (binary_search_slice elem vector-nums mid right)])))
+  (binary_search_slice elem vector-nums 0 (sub1 (vector-length vector-nums))))
