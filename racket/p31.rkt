6f600776ec54f87f2d254487e424d5809495abfa
hnng <nam@derivexperts.com>
2015-04-14 18:40:41 +0200

    ok

---
+++ racket/p31.rkt
@@ -0,0 +1,9 @@
+#lang racket
+(define (solve sum)
+  (define l '(1 2 5 10 20 50 100 200))
+  (define memo (make-hash))
+  (define (compute i)
+    (for/sum (map (lambda (j) (compute (- n j)))
+         (filter (lambda (j) (>= (- n j) 0)) l)))
+         
+               
