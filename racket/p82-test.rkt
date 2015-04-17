53b545ba389e5436918b23857be910e0a13f2e1a
hnng <nam@derivexperts.com>
2015-04-17 18:30:32 +0200

    ok

--- racket/p82-test.rkt
+++ racket/p82-test.rkt
@@ -8,6 +8,8 @@
     [d . ((e 6))]
     [e . ((f 9))]))
 
+(define hash-edges (compute-hash-edges edges))
+
 (check-equal? (add-one-distance '((b . ((5 . c) (1 . a))) 
                                   (c . ((2 . a))) 
                                   (d . ((1 . a)))
@@ -17,7 +19,9 @@
 
 (check-equal? (decompose  '(b . ((1 . a) (2 . c)))) '((b . (1 . a)) (b . (2 . c))))
 
-(check-equal? (next-node 'b edges) '((c 10)(d 15)))
+(check-equal? (list->set (next-node 'b hash-edges '((a . (0 . a))))) (list->set '((c 10)(d 15))))
+
+(check-equal? (list->set (next-node 'b hash-edges '())) (list->set '((a 7) (c 10)(d 15))))
 
 (check-equal? (new-distance '(b . (2 . a)) '((c . 1) (d . 2))) '((c . (3 . b))  (d . (4 . b))))
 
@@ -43,11 +47,8 @@
                                   (c . ((2 . a))) 
                                   (d . ((+inf.0 . a))))) 
               '(b . (1 . a))) 
+;(check-true (let ([ r (map (lambda (x) (list->set (hash->list x)))
+;                           (list (vector->edges #(0 1 2 3) 2 2) '#hash((0 . ((2 . 2) (1 . 1))) (1 . ((0 . 0) (3 . 3))) (2 . ((0 . 0) (3 . 3))) (3 . ((2 . 2) (1 . 1))))))])
+;              (equal? (car r) (cdr r))))
 
-(check-equal? (next-node
-   'f
-   '((a (b . 7) (c . 9) (f . 14))
-     (b (c . 10) (d . 15))
-     (c (d . 11) (f . 2))
-     (d (e . 6))
-     (e (f . 9)))) null)
+
--- racket/p82.rkt
+++ racket/p82.rkt
@@ -1,12 +1,15 @@
 #lang racket
 (require racket/trace)
 (require rackunit)
+(require math/matrix)
 (provide add-new-distance)
 (provide partial-shortest)
 (provide new-distance)
 (provide decompose)
 (provide next-node)
 (provide add-one-distance)
+(provide compute-hash-edges)
+(provide vector->edges)
 
 (define edges
   '([a . ((b . 7) (c . 9) (f . 14))]
@@ -25,6 +28,29 @@
     [f . ((g . 4))]
     ))
 
+(define (compute-hash-edges raw-edges)
+  (compute-hash-edges-loop (make-hash) raw-edges))
+
+(define (compute-hash-edges-loop results raw-edges)
+  (define (compute-hash-one results from to distance)
+    (define current-list
+      (if (hash-has-key? results from)
+          (hash-ref results from)
+          null))
+    (hash-set! results from (cons (cons to distance) current-list))
+    )
+  
+  (define (compute-hash-edge results edge)
+    (begin
+      (for-each (lambda (e) (compute-hash-one results (car e) (car edge) (cdr e))) (cdr edge))
+      (for-each (lambda (e) (compute-hash-one results (car edge) (car e) (cdr e))) (cdr edge))
+      (void)))
+  
+  (begin
+    (for-each (lambda (e) (compute-hash-edge results e)) raw-edges)
+    results))
+
+
 (define (partial-shortest l)
   (define elems (apply append (map decompose l)))
   (argmin (lambda (x) (car (cdr x))) elems))
@@ -32,12 +58,11 @@
 (define (decompose elem)
   (map (lambda (x) (cons (car elem) x)) (cdr elem)))
 
-(define (next-node symbol edges)
-  (let ([found (findf (lambda (x) (equal? (car x) symbol)) edges)])
-    (if found
-        (cdr found)
-        null
-        )))
+(define (next-node symbol hash-edges results)
+  (filter (lambda (e) (not (member (car e)
+                                   (map car results))))
+          
+          (hash-ref hash-edges symbol)))
 
 (define (new-distance chosen-node nexts)
   (map (lambda (x) (cons (car x) 
@@ -66,13 +91,69 @@
              [nr (cons m results)]
              [s (car m)]
              [removed-nodes (filter (lambda (x) (not (equal? (car x) s))) l)]
-             [nexts (next-node s edges)]
+             [nexts (next-node s edges nr)]
              [nd (new-distance m nexts)]
              [nl (add-new-distance removed-nodes nd)]
              )
         (new-step nl nr edges)
         )))
+
+;(new-step '((b . ((1 . a))) (c . ((2 . a)))) '((a . (0 . a))) (compute-hash-edges edges1))
+
+(define (dijkstra node-from graph)
+  (define origin-node (cons node-from (cons 0  node-from)))
+  (new-step (add-new-distance null 
+                              (new-distance origin-node
+                                            (next-node node-from graph null)))
+            (list origin-node)
+            graph))
+
+(define (vector->edges v-edges lmax cmax)
+  (define results (make-hash))
+  (define imax (sub1 lmax))
+  (define jmax (sub1 cmax))
+  (define (get-index i j)
+    (+ (* i cmax) j))
+  (define (get-val i j)
+    (vector-ref v-edges (get-index i j)))
+  (define (update-results i j func-i func-j)
+    (hash-set! results (get-index i j) 
+               (cons (cons (get-index (func-i i) (func-j j))
+                           (get-val (func-i i) (func-j j)))
+                     (hash-ref! results (get-index i j) null))))
+  
+  (for ([i (range lmax)])
+    (for ([j (range cmax)])
+      (when (< j jmax)
+        (update-results i j (lambda (x) x) add1))
+      (when (< i imax)
+        (update-results i j add1 (lambda (x) x)))
+      (when (> i 0)
+        (update-results i j sub1 (lambda (x) x)))
+      (when (> j 0)
+        (update-results i j (lambda (x) x) sub1))))
+  results
+  )
+
+(define (solve v lmax cmax)
+  (define (get-index i j)
+    (+ (* i cmax) j))
+  (define (solve-j j)
+    (solve-j (sub1 j))
+    (for ([i (range 0 imax)])
+      (define elem (vector-get v (get-index i j)))
+      (define mval (argmin (map (lambda (loop-i) (compute loop-i i j) 
+                                (range 0 lmax)))
+      (vector-set! v (get-index i j) (cons  (car elem) mval))
+      
+    
+
+
+
+
 (trace new-step)
+(trace partial-shortest)
 ;
 ;
 ;(check-equal? (partial-shortest '((b . (2 . a)) (d . (3 . b)))) '(b . (2 . a)))
+
